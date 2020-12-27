9164c565b1df7cea7cf82bb60d2c7c27cf6ff3b7
Erwan Leroy <eleroy@spinvfx.com>
2020-12-27 17:25:26 +1300

    WIP: Crashy threading implementation

--- dag_capture.py
+++ dag_capture.py
@@ -6,6 +6,23 @@ from Qt import QtWidgets, QtOpenGL, QtGui, QtCore
 from math import ceil
 
 
+def get_dag():
+    stack = QtWidgets.QApplication.topLevelWidgets()
+    while stack:
+        widget = stack.pop()
+        if widget.objectName() == 'DAG.1':
+            for c in widget.children():
+                if isinstance(c, QtOpenGL.QGLWidget):
+                    return c
+        stack.extend(c for c in widget.children() if c.isWidgetType())
+
+
+def grab_dag(dag, painter, xpos, ypos):
+    dag.updateGL()  # This does some funky back and forth but function grabs the wrong thing without it
+    pix = dag.grabFrameBuffer()
+    painter.drawImage(xpos, ypos, pix)
+
+
 class DagCapturePanel(QtWidgets.QDialog):
     def __init__(self):
         super(DagCapturePanel, self).__init__()
@@ -16,7 +33,9 @@ class DagCapturePanel(QtWidgets.QDialog):
             raise RuntimeError("Couldn't get DAG widget")
 
         self.dag_bbox = None
-        self.capture_size = None
+        # self.capture_size = None
+        self.capture_thread = DagCapture()
+        self.capture_thread.finished.connect(self.show_finished_popup)
 
         # UI
         self.setWindowTitle("DAG Capture options")
@@ -96,8 +115,14 @@ class DagCapturePanel(QtWidgets.QDialog):
         info_box.setFrameStyle(info_box.StyledPanel)
         info_box.setLayout(QtWidgets.QVBoxLayout())
         info_box.layout().addWidget(self.info)
-
         main_layout.addWidget(info_box)
+
+        # Buttons
+        button_box = QtWidgets.QDialogButtonBox(QtWidgets.QDialogButtonBox.Ok | QtWidgets.QDialogButtonBox.Cancel)
+        button_box.accepted.connect(self.do_capture)
+        button_box.rejected.connect(self.reject)
+        main_layout.addWidget(button_box)
+
         self.setLayout(main_layout)
 
         self.inspect_dag()
@@ -105,7 +130,13 @@ class DagCapturePanel(QtWidgets.QDialog):
     def display_info(self):
         zoom = self.zoom_level.value()
         # Check the size of the current widget, excluding the right side (because of minimap)
-        capture_width = max(self.dag.width() - self.ignore_right.value(), 1)
+        capture_width = self.dag.width()
+        crop = self.ignore_right.value()
+        if crop >= capture_width:
+            self.info.setText("Error: Crop is larger than capture area.\n"
+                              "Increase DAG size or reduce crop.")
+            return
+        capture_width -= crop
         capture_height = self.dag.height()
 
         # Calculate the number of tiles required to cover all
@@ -125,7 +156,6 @@ class DagCapturePanel(QtWidgets.QDialog):
         self.info.setText(info)
 
     def inspect_dag(self):
-        print self.capture.currentIndex()
         nodes = nuke.allNodes() if self.capture.currentIndex() == 0 else nuke.selectedNodes()
         # Calculate the total size of the DAG
         min_x = min([node.xpos() for node in nodes])
@@ -140,42 +170,46 @@ class DagCapturePanel(QtWidgets.QDialog):
         filename, _filter = QtWidgets.QFileDialog.getSaveFileName(parent=self, caption='Select output file',
                                                                   filter="PNG Image (*.png)")
         self.path.setText(filename)
+        print filename, type(filename)
+
+    def do_capture(self):
+        print "doing capture"
+        # Check path is valid and writable
+        # Push settings to Thread
+        self.capture_thread.path = self.path.text()
+        self.capture_thread.margins = self.margins.value()
+        self.capture_thread.ignore_right = self.ignore_right.value()
+        self.capture_thread.delay = self.delay.value()
+        self.capture_thread.bbox = self.dag_bbox
+        self.capture_thread.zoom = self.zoom_level.value()
+        # Run thread
+        print "init complete"
+        self.capture_thread.start()
+        pass
+
+    def show_finished_popup(self):
+        nuke.message("Capture complete:\n"
+                     "{}".format(self.path.text()))
 
 
-def get_dag():
-    stack = QtWidgets.QApplication.topLevelWidgets()
-    while stack:
-        widget = stack.pop()
-        if widget.objectName() == 'DAG.1':
-            for c in widget.children():
-                if isinstance(c, QtOpenGL.QGLWidget):
-                    return c
-        stack.extend(c for c in widget.children() if c.isWidgetType())
-
-
-def grab_dag(dag, painter, xpos, ypos):
-    dag.updateGL()  # This does some funky back and forth but function grabs the wrong thing without it
-    pix = dag.grabFrameBuffer()
-    painter.drawImage(xpos, ypos, pix)
-
-
-class DagCapture(threading.Thread):
-    def __init__(self, path, margins=20, ignore_right=200):
+class DagCapture(QtCore.QThread):
+    def __init__(self, path='', margins=20, ignore_right=200, delay=0.3, bbox=(-50, 50, -50, 50), zoom=1.0):
         super(DagCapture, self).__init__()
         self.path = path
         self.margins = margins
         self.ignore_right = ignore_right
-        self.delay = 0.2
+        self.delay = delay
+        self.bbox = bbox
+        self.zoom = zoom
 
     def run(self):
+        print "In thread"
         # Store the current dag size and zoom
         original_zoom = nuke.zoom()
         original_center = nuke.center()
         # Calculate the total size of the DAG
-        min_x = min([node.xpos() for node in nuke.allNodes()]) - self.margins
-        min_y = min([node.ypos() for node in nuke.allNodes()]) - self.margins
-        max_x = max([node.xpos() + node.screenWidth() for node in nuke.allNodes()]) + self.margins
-        max_y = max([node.ypos() + node.screenHeight() for node in nuke.allNodes()]) + self.margins
+        min_x, min_y, max_x, max_y = self.dag_bbox
+        zoom = self.zoom
 
         # Get the Dag Widget
         dag = get_dag()
@@ -187,10 +221,12 @@ class DagCapture(threading.Thread):
         capture_height = dag.height()
 
         # Calculate the number of tiles required to cover all
-        image_width = max_x - min_x
-        image_height = max_y - min_y
-        horizontal_tiles = int(ceil(image_width / float(capture_width)))
-        vertical_tiles = int(ceil(image_height / float(capture_height)))
+        image_width = (max_x - min_x) * zoom + self.margins * 2
+        image_height = (max_y - min_y) * zoom + self.margins * 2
+        capture_image_width = (max_x - min_x) * zoom + (self.margins * 2 / zoom)
+        capture_image_height = (max_y - min_y) * zoom + (self.margins * 2 / zoom)
+        horizontal_tiles = int(ceil(capture_image_width / float(capture_width)))
+        vertical_tiles = int(ceil(capture_image_height / float(capture_height)))
         # Create a pixmap to store the results
         pixmap = QtGui.QPixmap(image_width, image_height)
         painter = QtGui.QPainter(pixmap)
